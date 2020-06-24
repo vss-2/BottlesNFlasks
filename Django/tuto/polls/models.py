7bc892f0ba4daecb13cb564085d09767918d2463
Vitor Sousa <vss2@cin.ufpe.br>
2020-06-24 16:11:54 -0300

    Tutorial do Django
    Sistema de votacao baseado no tutorial disponivel em: https://docs.djangoproject.com

---
+++ Django/tuto/polls/models.py
@@ -0,0 +1,29 @@
+import datetime
+from django.db import models
+from django.utils import timezone
+
+class Question(models.Model):
+
+        def __str__(self):
+                return self.txt_pergunta
+        # Adicionamos o esta def para quando perguntarmos ao
+        # banco de dados ele responder algo diferente de 'Question object'
+
+        def bool_foi_pub_hoje(self):
+                return self.data_publicacao >= timezone.now() - datetime.timedelta(days=1)
+
+        txt_pergunta    = models.CharField(max_length=200)
+        data_publicacao = models.DateTimeField('date published')
+        # O primeiro parâmetro "date published" é para 
+        # tornar legível e mais fácil documentação
+
+class Choice(models.Model):
+
+        def __str__(self):
+                return self.escolha
+
+        pergunta = models.ForeignKey(Question, on_delete=models.CASCADE)
+        escolha  = models.CharField(max_length=200)
+        votos    = models.IntegerField(default=0)
+        # A foreign key é usada para os tipos de relacionamentos
+        # Muitos para um, muitos para muitos e um para um
